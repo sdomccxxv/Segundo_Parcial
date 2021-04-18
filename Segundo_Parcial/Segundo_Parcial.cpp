63c92da81bc578f3331b2738f64764de4d3836ec
Samuel Ortiz <samuel.om@alliedglobalbpo.com>
2021-04-17 22:57:30 -0600

    Creación de menu

--- Segundo_Parcial/Segundo_Parcial.cpp
+++ Segundo_Parcial/Segundo_Parcial.cpp
@@ -2,18 +2,30 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n..";
+    int a;
+    cout << "Si desea\nIngresar, modificar o consultar datos de puestos presione 1 \nIngresar, modificar o consultar datos de empleados presione 2: ";
+    cin >> a;
+
+    switch (a)
+    {
+    case 1:
+        int p;
+        cout << "Si desea\ninsertar presione 1\n modificar presione 2 \n consultar presione 3: ";
+        cin >> p;
+
+    
+    case 2:
+        int e;
+        cout << "Si desea\ninsertar presione 1\n modificar presione 2 \n consultar presione 3: ";
+        cin >> e;
+
+      }
+
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
