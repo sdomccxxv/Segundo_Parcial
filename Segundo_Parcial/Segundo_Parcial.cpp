4c0e5cc37186cc993e0f1309e5e5f0e20b9ea89b
Samuel Ortiz <samuel.om@alliedglobalbpo.com>
2021-04-17 23:06:56 -0600

    Funcion ingresar puesto

--- Segundo_Parcial/Segundo_Parcial.cpp
+++ Segundo_Parcial/Segundo_Parcial.cpp
@@ -4,8 +4,12 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <mysql.h>
 
 using namespace std;
+int q_estado;
+
+void insertar_puesto();
 
 int main()
 {
@@ -20,6 +24,13 @@ int main()
         cout << "Si desea\ninsertar presione 1\n modificar presione 2 \n consultar presione 3: ";
         cin >> p;
 
+        switch (p)
+        {
+        case 1:
+            insertar_puesto(); break;
+        default:
+            break;
+        }
     
     case 2:
         int e;
@@ -27,5 +38,33 @@ int main()
         cin >> e;
 
       }
+
+}
+
+void insertar_puesto() {
+    MYSQL* conectar;
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "root", "150289", "db_parcial2", 3306, NULL, 0);
+    if (conectar) {
+        string puesto;
+        cout << "Ingrese puesto: ";
+        cin >> puesto;
+
+        string insertar = "INSERT INTO puestos(puesto) VALUES ('" + puesto + "');";
+
+        const char* i = insertar.c_str();
+        q_estado = mysql_query(conectar, i);
+
+        if (!q_estado) {
+            cout << "Ingreso de marca exitoso..." << endl;
+        }
+        else {
+            cout << "Error al ingresar marca..." << endl;
+            cout << insertar << endl << mysql_error(conectar) << endl;
+        }
+    }
+    else {
+        cout << "Conexion fallida..." << endl;
+    }
 
 }
