62950a14e4bbb08588f296757919864eaeafbb98
Samuel Ortiz <samuel.om@alliedglobalbpo.com>
2021-04-18 11:15:46 -0600

    Se agregan funciones de mostrar, actualizar y eliminar puesto.

--- Segundo_Parcial/Segundo_Parcial.cpp
+++ Segundo_Parcial/Segundo_Parcial.cpp
@@ -1,6 +1,3 @@
-// Segundo_Parcial.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -10,6 +7,9 @@ using namespace std;
 int q_estado;
 
 void insertar_puesto();
+void mostrar_puesto();
+void actualizar_puesto();
+void eliminar_puesto();
 
 int main()
 {
@@ -21,16 +21,23 @@ int main()
     {
     case 1:
         int p;
-        cout << "Si desea\ninsertar presione 1\n modificar presione 2 \n consultar presione 3: ";
+        cout << "Si desea\ninsertar presione 1\n modificar presione 2 \n consultar presione 3 \n eliminar presione 4:";
         cin >> p;
 
         switch (p)
         {
         case 1:
             insertar_puesto(); break;
+        case 2:
+            actualizar_puesto(); break;
+        case 3: 
+            mostrar_puesto(); break;
+        case 4:
+            eliminar_puesto(); break;
         default:
             break;
         }
+        break;
     
     case 2:
         int e;
@@ -56,7 +63,7 @@ void insertar_puesto() {
         q_estado = mysql_query(conectar, i);
 
         if (!q_estado) {
-            cout << "Ingreso de marca exitoso..." << endl;
+            cout << "Ingreso de puesto exitoso..." << endl;
         }
         else {
             cout << "Error al ingresar marca..." << endl;
@@ -66,5 +73,98 @@ void insertar_puesto() {
     else {
         cout << "Conexion fallida..." << endl;
     }
+    return;
 
+}
+
+void mostrar_puesto() {
+    MYSQL* conectar;
+    MYSQL_ROW fila;
+    MYSQL_RES* resultado;
+
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "root", "150289", "db_parcial2", 3306, NULL, 0);
+    if (conectar) {
+        string consulta = "SELECT * FROM puestos";
+        const char* c = consulta.c_str();
+        q_estado = mysql_query(conectar, c);
+
+        if (!q_estado) {
+            resultado = mysql_store_result(conectar);
+            while (fila = mysql_fetch_row(resultado)) {
+                cout << fila[0] << " - " << fila[1] << endl;
+            }
+        }
+        else {
+            cout << "Error al consultar..." << endl;
+        }
+
+    }
+    else {
+        cout << "Conexion fallida..." << endl;
+    }
+    system("pause");
+}
+
+void actualizar_puesto() {
+    MYSQL* conectar;
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "root", "150289", "db_parcial2", 3306, NULL, 0);
+    if (conectar) {
+        //cout << "Conexion exitosa..." << endl;
+        char idpuesto;
+        string puesto;
+        cout << "Ingrese id que desea modificar: ";
+        cin >> idpuesto;
+
+        cout << "Ingrese nuevo puesto: ";
+        cin >> puesto;
+
+        string update = "UPDATE puestos SET puesto = '" + puesto + "' WHERE idpuesto = " + idpuesto + "";
+        const char* i = update.c_str();
+        q_estado = mysql_query(conectar, i);
+
+        if (!q_estado) {
+            cout << "Actualizacion exitosa..." << endl;
+        }
+        else {
+            cout << "Error al actualizar..." << endl;
+            cout << update << endl << mysql_error(conectar) << endl;
+        }
+    }
+    else {
+        cout << "Conexion fallida..." << endl;
+    }
+    system("pause");
+    system("exit");
+}
+
+void eliminar_puesto() {
+    MYSQL* conectar;
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "root", "150289", "db_parcial2", 3306, NULL, 0);
+    if (conectar) {
+
+        string idpuesto;
+        cout << "Ingrese id que desea eliminar: ";
+        cin >> idpuesto;
+
+        string eliminar = "DELETE FROM puestos WHERE idpuesto = " + idpuesto +"\n";
+        cout << eliminar;
+        const char* i = eliminar.c_str();
+        q_estado = mysql_query(conectar, i);
+
+        if (!q_estado) {
+            cout << "Eliminacion exitosa..." << endl;
+            return;
+        }
+        else {
+            cout << "Error al eliminar..." << endl;
+            cout << eliminar << endl << mysql_error(conectar) << endl;
+        }
+    }
+    else {
+        cout << "Conexion fallida..." << endl;
+    }
+    
 }
